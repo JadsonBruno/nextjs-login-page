78556273e9b46c91f41be945b9de71504e347145
Jadson <jadsombruno2016@gmail.com>
2021-02-14 07:50:28 -0300

    Add general app component

---
+++ login-challenge/src/pages/_app.tsx
@@ -0,0 +1,32 @@
+/**
+ * IMPORTS
+ */
+import {ThemeProvider} from 'styled-components';
+import GlobalStyles from '../styles/global';
+import {Provider} from 'react-redux';
+import reducers from '../aggregates/index';
+import Store from '../store/index';
+import {theme} from '../styles/global';
+import '../styles/montserrat.css';
+import 'antd/dist/antd.css';
+
+// combine reducers
+const reducer = Store.combineReducers(reducers);
+
+// create global redux store
+const store = Store.create(reducer);
+
+
+/**
+ * EXPORTS
+ */
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
+}
